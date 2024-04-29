73fba6289df42e5823e52be4baa61969c0b808c5
Kevin-Oudai <kevinoudai@hotmail.com>
2024-04-29 10:08:25 -0400

    Update file input output

--- FileInputOutput.cpp
+++ FileInputOutput.cpp
@@ -1,3 +1,4 @@
+/*
 #include <iostream>
 #include <string>
 #include <fstream>
@@ -6,13 +7,13 @@ struct Student
 {
 	int id;
 	std::string name;
-	double gpa;
+	std::string gpa;
 };
 
 int main()
 {
 	// Create an instance of the student struct
-	Student s1 = { 123, "Jonathan Brown", 4.0 };
+	Student s1 = { 123, "Jonathan Brown", "4.0" };
 
 	// Write struct data to a file
 	std::ofstream outfile("student.txt");
@@ -33,6 +34,55 @@ int main()
 	{
 		Student s2;
 		infile >> s2.id >> s2.name >> s2.gpa;
+		std::cout << "Read data from file: " << std::endl;
+		std::cout << "Student ID: " << s2.id << std::endl;
+		std::cout << "Student Name: " << s2.name << std::endl;
+		std::cout << "Student GPA: " << s2.gpa << std::endl;
+		infile.close();
+	}
+	else
+	{
+		std::cerr << "Unable to open file for reading." << std::endl;
+	}
+
+	return 0;
+}
+*/
+
+#include <iostream>
+#include <fstream>
+#include <string>
+
+struct Student
+{
+	int id;
+	std::string name;
+	double gpa;
+};
+
+int main()
+{
+	Student s1 = { 123, "Jonathan Brown", 3.5 };
+
+	// Write the struct data to a file
+	std::ofstream outfile("student.txt", std::ios::binary);
+	if (outfile.is_open())
+	{
+		outfile.write(reinterpret_cast<char*>(&s1), sizeof(Student));
+		outfile.close();
+		std::cout << "Data written to file successfully." << std::endl;
+	}
+	else
+	{
+		std::cerr << "Unable to open file for writing." << std::endl;
+	}
+
+	// Read the struct data from a file
+	std::ifstream infile("student.txt", std::ios::binary);
+	if (infile.is_open())
+	{
+		Student s2;
+		infile.read(reinterpret_cast<char*>(&s2), sizeof(Student));
 		std::cout << "Read data from file: " << std::endl;
 		std::cout << "Student ID: " << s2.id << std::endl;
 		std::cout << "Student Name: " << s2.name << std::endl;
